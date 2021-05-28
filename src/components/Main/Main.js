c4cde59bff7da8a9974efe96a9dca434674b50d1
Alexandr <raspertovalexandr@yandex.ru>
2021-05-28 22:15:06 +0300

    edit footer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Button, Grid, makeStyles } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 import computer from '../../images/main/computer.png';
 import phone from '../../images/main/phone.png';
 import './Main.css';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
+  main: {
+    background: `url(${phone}) 1135px 250px no-repeat, url(${computer}) 870px 90px no-repeat`,
+    minHeight: 'calc(100vh - 92px)',
+    boxSizing: 'border-box',
+    [theme.breakpoints.down('xs')]: {
+      background: `url(${phone}) 120% 60px / 50% no-repeat, url(${computer}) 20px 50px / 65% no-repeat`,
+      minHeight: 'calc(100vh - 74px)',
+      padding: '75% 20px 50px',
+    },
+  },
   button: {
-    height: 50,
+    minHeight: 50,
     fontWeight: 500,
     fontSize: 20,
-    lineHeight: 24,
+    lineHeight: '24px',
     borderRadius: 5,
     backgroundColor: '#DA125A',
     fontFamily: '"Inter", sans-serif',
@@ -21,6 +31,14 @@ const useStyles = makeStyles(() => ({
     '&:hover': {
       backgroundColor: '#FF186B',
     },
+    [theme.breakpoints.down('xs')]: {
+      minHeight: 45,
+      maxWidth: 300,
+      width: '100%',
+      fontWeight: 400,
+      fontSize: 16,
+      lineHeight: '20px',
+    },
   },
 }));
 
@@ -28,31 +46,26 @@ function Main({ openPopup }) {
   const classes = useStyles();
 
   return (
-    <Grid
-      container
-      component='main'
-      justify='space-between'
-      alignItems='center'
-      className='content'
-      style={{ background: `url(${phone}) 1135px 260px no-repeat, url(${computer}) 870px 90px no-repeat` }}
-    >
-      <div>
-        <h1 className='title'>
-          Генератор&nbsp;
-          <span className='title text_turquoise'>документов&nbsp;</span>
-          с гибкими&nbsp;шаблонами
-        </h1>
-        <p className='lists'>Создание и управление шаблонами, автораспознавание документов, удобное хранилище,&nbsp;интеграция с CRM</p>
-        <Button
-          variant='contained'
-          color='primary'
-          className={classes.button}
-          onClick={openPopup}
-        >
-          Попробовать 14 дней бесплатно
-        </Button>
+    <main className={classes.main}>
+      <div className='main__container'>
+        <div>
+          <h1 className='title'>
+            Генератор&nbsp;
+            <span className='title text_turquoise'>документов&nbsp;</span>
+            с гибкими&nbsp;шаблонами
+          </h1>
+          <p className='subtitle'>Создание и управление шаблонами, автораспознавание документов, удобное хранилище,&nbsp;интеграция с CRM</p>
+          <Button
+            variant='contained'
+            color='primary'
+            className={classes.button}
+            onClick={openPopup}
+          >
+            Попробовать 14 дней бесплатно
+          </Button>
+        </div>
       </div>
-    </Grid>
+    </main>
   );
 }
 
